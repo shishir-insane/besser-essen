26f3500137539469d6b911fe7443d9bea3218ab9
shishir-insane <shishir.insane@gmail.com>
2019-09-06 11:07:18 +0200

    Removed unused files

--- wp-content/themes/besser-essen/extend/php-variables.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Insert your custom vars here and back up this file before a theme update,
-// so you can override color scheme variables and not lose your changes after theme update
-
-// e.g
-// $scheme_vars[ 'bodyBackgroundColor' ]         = '#f0f3f5';
-
-?>
